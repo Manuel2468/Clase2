9228b7c321ce80b8e0472c79de7a3641c60f8ef5
Manuel Romero <romero__manuel@hotmail.com>
2020-09-21 18:05:10 -0500

    Agrego salario devengado

--- Empleado.py
+++ Empleado.py
@@ -2,24 +2,40 @@
 class Empleado:
 
     def __init__(self):
-        self.nombre = None #string o texto
-        self.apellido = None #string o texto
-        self.sueldo = None #float o decimal
+        self.__nombre = None #string o texto
+        self.__apellido = None #string o texto
+        self.__sueldo = None #float o decimal
+        self.__diasliquidados = None #int o entero
 
     def getNombre(self):
-        return self.nombre
+        return self.__nombre
 
     def getApellido(self):
-        return self.apellido
+        return self.__apellido
 
     def getSueldo(self):
-        return self.sueldo
+        return self.__sueldo
+
+    def getDiasLiquidados(self):
+        return self.__diasliquidados
 
     def setNombre(self, nombre:str):
-        self.nombre = nombre
+        self.__nombre = nombre
 
     def setApellido(self, apellido:str):
-        self.apellido = apellido
+        self.__apellido = apellido
     
     def setSueldo(self, sueldo:float):
-        self.sueldo = sueldo
+        self.__sueldo = sueldo
+
+    def setDiasLiquidados(self, dias:int):
+        self.__diasliquidados = dias
+
+    def SalarioDevengado(self):
+        return self.__sueldo * int(self.__diasliquidados) / 30
+
+    def __str__(self):
+        return str('\nNombre: ' + self.__nombre +
+                '\nApellido: ' + self.__apellido +
+                '\nSueldo: ' + str(self.__sueldo) +
+                '\nSalario Devengado:' + str(self.SalarioDevengado()))
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from Empleado import *
 
+empleados = []
+
+
+
 e1 = Empleado()
 
 e1.setNombre('Manuel')
 e1.setApellido('Romero')
 e1.setSueldo(1000000)
+e1.setDiasLiquidados(10)
+empleados.append(e1) #Ingreso de informacion en el arreglo
 
+e2 = Empleado()
+
+e2.setNombre('Angelica')
+e2.setApellido('Franco Rojas')
+e2.setSueldo(2500000)
+e2.setDiasLiquidados(30)
+empleados.append(e2) #Ingreso de informacion en el arreglo
+
+
+for i in empleados:
+    print('\n',i)
+
+'''
 print(e1.getNombre())
 print(e1.getApellido())
 print(e1.getSueldo())
+'''
